18ed24c7374f15c8d9cdda7c08d8bb238c2c4541
András Németh <andras.nemeth4@logmein.com>
2022-03-13 21:47:21 +0100

    persist state

---
+++ app/src/main/java/hu/kts/wtracker/prefkeys.kt
@@ -0,0 +1,7 @@
+package hu.kts.wtracker
+
+const val KEY_WORK_TIME = "workTime"
+const val KEY_REST_TIME = "restTime"
+const val KEY_IS_RUNNING = "isRunning"
+const val KEY_LAST_PERIOD_SWITCH = "lastPeriodSwitch"
+const val KEY_PERIOD = "period"
--- app/src/main/java/hu/kts/wtracker/ui/main/MainViewModel.kt
+++ app/src/main/java/hu/kts/wtracker/ui/main/MainViewModel.kt
@@ -1,11 +1,11 @@
 package hu.kts.wtracker.ui.main
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import hu.kts.wtracker.R
-import hu.kts.wtracker.Timer
-import hu.kts.wtracker.WTrackerApp
+import hu.kts.wtracker.*
+import java.util.concurrent.TimeUnit
 
 class MainViewModel : ViewModel() {
 
@@ -15,6 +15,7 @@ class MainViewModel : ViewModel() {
 
     private val timer = Timer()
     private val context = WTrackerApp.instance.applicationContext
+    private val preferences = context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
 
     private var workSec = 0
     private var restSec = 0
@@ -22,6 +23,7 @@ class MainViewModel : ViewModel() {
     private var isRunning = false
 
     init {
+        restoreState()
         updateViewState()
     }
 
@@ -32,6 +34,7 @@ class MainViewModel : ViewModel() {
             timer.start { onTimerTick() }
         }
         isRunning = !isRunning
+        persistState()
         updateViewState()
     }
 
@@ -39,6 +42,8 @@ class MainViewModel : ViewModel() {
         return if (!isRunning) {
             workSec = 0
             restSec = 0
+            period = Period.WORK
+            persistState()
             updateViewState()
             true
         } else {
@@ -48,6 +53,7 @@ class MainViewModel : ViewModel() {
 
     fun onScreenTouch() {
         period = if (period == Period.WORK) Period.REST else Period.WORK
+        persistState()
         updateViewState()
     }
 
@@ -66,9 +72,47 @@ class MainViewModel : ViewModel() {
         ))
     }
 
+    private fun persistState() {
+        preferences.edit().apply {
+            putInt(KEY_WORK_TIME, workSec)
+            putInt(KEY_REST_TIME, restSec)
+            putLong(KEY_LAST_PERIOD_SWITCH, System.currentTimeMillis())
+            putBoolean(KEY_IS_RUNNING, isRunning)
+            putString(KEY_PERIOD, period.toString())
+        }.apply()
+    }
+
+    private fun restoreState() {
+        isRunning = preferences.getBoolean(KEY_IS_RUNNING, false)
+        period = Period.safeValueOf(preferences.getString(KEY_PERIOD, ""))
+        workSec = preferences.getInt(KEY_WORK_TIME, 0)
+        restSec = preferences.getInt(KEY_REST_TIME, 0)
+        if (isRunning) {
+            val lastPeriodSwitch = preferences.getLong(KEY_LAST_PERIOD_SWITCH, 0)
+            val elapsedSecs = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - lastPeriodSwitch).toInt()
+            if (period == Period.WORK) {
+                workSec += elapsedSecs
+            } else {
+                restSec += elapsedSecs
+            }
+            timer.start { onTimerTick() }
+        }
+
+    }
+
     data class ViewState(val work: String, val rest: String, val buttonText: String, val period: Period, val isRunning: Boolean)
 
     enum class Period {
-        WORK, REST
+        WORK, REST;
+
+        companion object {
+            fun safeValueOf(value: String?): Period {
+                return try {
+                    valueOf(value ?: "")
+                } catch (e: IllegalArgumentException) {
+                    WORK
+                }
+            }
+        }
     }
 }
