9021ba8372b195e217b97a32af309c0fd13d76c9
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-10 21:18:52 +0100

    setup compose

---
+++ app/src/main/java/hu/kts/wtracker/ui/main/MainScreen.kt
@@ -0,0 +1,20 @@
+package hu.kts.wtracker.ui.main
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TimeSegment() {
+    Text("Hello World!")
+}
+
+
+@Preview
+@Composable
+fun Preview() {
+    MaterialTheme {
+        TimeSegment()
+    }
+}
