3f301130b37a196b9ede2a1ba775fc6595d36cbf
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-10 23:01:08 +0100

    create time segment

--- app/src/main/java/hu/kts/wtracker/ui/main/MainScreen.kt
+++ app/src/main/java/hu/kts/wtracker/ui/main/MainScreen.kt
@@ -1,13 +1,38 @@
 package hu.kts.wtracker.ui.main
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+
 @Composable
-fun TimeSegment() {
-    Text("Hello World!")
+fun TimeSegment(
+    label: String,
+    primary: String,
+    secondary: String,
+    onClick: () -> Unit = {}
+) {
+    Column(Modifier.clickable(onClick = onClick)) {
+        Text(
+            label,
+            style = MaterialTheme.typography.titleSmall,
+        )
+        Text(
+            primary,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+        )
+        Text(
+            secondary,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.align(alignment = Alignment.End)
+        )
+    }
 }
 
 
@@ -15,6 +40,10 @@ fun TimeSegment() {
 @Composable
 fun Preview() {
     MaterialTheme {
-        TimeSegment()
+        TimeSegment(
+            label = "Work",
+            primary = "0:00:00",
+            secondary = "0:00:00",
+        )
     }
 }
