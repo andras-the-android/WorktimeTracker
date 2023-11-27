1ab38620371d0db0bc13af79d8a2f089bd6d05ac
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-27 14:04:54 +0100

    comment out MainFragment partially

--- app/src/main/java/hu/kts/wtracker/ui/main/MainFragment.kt
+++ app/src/main/java/hu/kts/wtracker/ui/main/MainFragment.kt
@@ -21,26 +21,26 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentMainBinding.inflate(layoutInflater)
-        binding.btnStart.setOnClickListener { viewModel.onStartButtonClicked() }
-        binding.btnStart.setOnLongClickListener { viewModel.onStartButtonLongClicked() }
-        binding.btnNotificationFrequency.setOnClickListener { viewModel.onNotificationFrequencyButtonClicked() }
-        binding.twWork.setOnClickListener { viewModel.onScreenTouch() }
-        binding.twRest.setOnClickListener { viewModel.onScreenTouch() }
+//        binding.btnStart.setOnClickListener { viewModel.onStartButtonClicked() }
+//        binding.btnStart.setOnLongClickListener { viewModel.onStartButtonLongClicked() }
+//        binding.btnNotificationFrequency.setOnClickListener { viewModel.onNotificationFrequencyButtonClicked() }
+//        binding.twWork.setOnClickListener { viewModel.onScreenTouch() }
+//        binding.twRest.setOnClickListener { viewModel.onScreenTouch() }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.state.observe(viewLifecycleOwner) { viewState ->
-            binding.twWork.text = viewState.work
-            binding.twRest.text = viewState.rest
-            binding.twWorkSegment.text = viewState.workSegment
-            binding.twRestSegment.text = viewState.restSegment
-            binding.btnStart.text = viewState.buttonText
-            binding.root.setBackgroundColor(resources.getColor(viewState.period.color, null))
-            binding.btnNotificationFrequency.text = getString(viewState.notificationFrequency.label)
-            keepScreenAwake(viewState.period.isRunning())
-        }
+//        viewModel.state.observe(viewLifecycleOwner) { viewState ->
+//            binding.twWork.text = viewState.work
+//            binding.twRest.text = viewState.rest
+//            binding.twWorkSegment.text = viewState.workSegment
+//            binding.twRestSegment.text = viewState.restSegment
+//            binding.btnStart.text = viewState.buttonText
+//            binding.root.setBackgroundColor(resources.getColor(viewState.period.color, null))
+//            binding.btnNotificationFrequency.text = getString(viewState.notificationFrequency.label)
+//            keepScreenAwake(viewState.period.isRunning())
+//        }
     }
 
     private fun keepScreenAwake(keep: Boolean) {
