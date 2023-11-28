ea26e81b7f301850a4b7fbdacc21ab1bf62ffa8c
Andras Nemeth <andras.nemeth@lastpass.com>
2023-11-28 20:36:23 +0100

    fix skip dialog loosing state

--- app/src/main/java/hu/kts/wtracker/ui/main/dialogs.kt
+++ app/src/main/java/hu/kts/wtracker/ui/main/dialogs.kt
@@ -15,7 +15,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -77,7 +77,7 @@ fun SkipNotificationsDialog(
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.Start,
             ) {
-                var selectedOption by remember {
+                var selectedOption by rememberSaveable {
                     mutableIntStateOf(-1)
                 }
 
